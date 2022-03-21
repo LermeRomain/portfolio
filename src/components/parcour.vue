6800ddbf625f088cfc5c82900ca90be264165154
romai <romainlerme@hotmail.fr>
2022-03-21 09:52:40 +0100

    maj card

--- src/components/parcour.vue
+++ src/components/parcour.vue
@@ -3,10 +3,7 @@
     <div class="columns">
       <div class="column"></div>
       <div class="column">
-        <a class="btn-download" href="[../assets/CV%20Romain%20Lerme.pdf]"> Télécharger</a>
-        <a href="http://www.alsacreations.com/astuce/lire/1603-html5-attribut-download.html"
-           download="../assets/CV%20Romain%20Lerme.pdf">Télécharger l'article en TXT</a>
-        <div class="column"></div>
+        <a class="btn" href="src/assets/CV Romain Lerme.pdf" download> Télécharger</a>
       </div>
     </div>
   </div>
